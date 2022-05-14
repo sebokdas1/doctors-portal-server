ff996171a40507f59b1cfa55ca157059f3952518
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-14 12:01:54 +0600

    get specefice date

--- index.js
+++ index.js
@@ -26,8 +26,33 @@ async function run() {
             res.send(services);
         });
 
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+            //get all services
+            const services = await serviceCollection.find().toArray();
+            //get all booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+            //for each service
+            services.forEach(service => {
+                //find booking for that service
+                const serviceBookings = bookings.filter(book => book.treatement === service.name);
+                //select slots for the service booking
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                //select those slots that are not in bookedSlots
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                service.slots = available;
+            });
+            res.send(services);
+        });
+
+        // Warning: This is not the proper way to query multiple collection. 
+        // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
+
+
         app.post('/booking', async (req, res) => {
             const booking = req.body;
+            //for find patient same service booking at same date
             const query = { treatement: booking.treatement, date: booking.date, patient: booking.patient }
             const exists = await bookingCollection.findOne(query);
             if (exists) {
