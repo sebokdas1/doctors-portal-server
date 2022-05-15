47cfad1fa77d08355559a308f6ceb0cfc5096749
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-15 15:24:32 +0600

    verify user admin

--- index.js
+++ index.js
@@ -69,6 +69,36 @@ async function run() {
         });
 
 
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        });
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAcct = await userCollection.findOne({ email: requester });
+            if (requesterAcct.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                }
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden' })
+            }
+
+        });
+
         app.put('/user/:email', async (req, res) => {
             const user = req.body;
             const email = req.params.email;
